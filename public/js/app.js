7313ff9f3b9d893bc96cdf6bdcf226b59232edd7
Gio <jojuagio7@gmail.com>
2021-10-22 02:24:58 +0400

    deploy to surge

--- public/js/app.js
+++ public/js/app.js
@@ -151,7 +151,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkLord_of_the_Rings"] = self["webpackChunkLord_of_the_Rings"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunklordoftherings"] = self["webpackChunklordoftherings"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
