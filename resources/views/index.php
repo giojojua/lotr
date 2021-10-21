ae99364dde4551748d853b50358b07e99a4eb579
gio <jojuagio7@gmail.com>
2021-10-21 18:52:07 +0400

    Markup Final Project Configuration

---
+++ resources/views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta charset="utf-8">
+<title>fsociety</title>
+<link rel="stylesheet" href="">
+<body>
+<div class="container">
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+    <p>This is another paragraph.</p>
+</div>
+<script src="./js/app.js"></script>
+</body>
+</html>
